e2722f2aef6e14e0bc8069312928ac33e5d1d98c
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-07-25 01:05:05 +0530

    mongoose server and rest api

---
+++ server/server.js
@@ -0,0 +1,39 @@
+var {mongoose}=require('./db/mongoose');
+var {Todo}=require('./models/todo');
+var express=require('express');
+var bodyParser=require('body-parser');
+
+var app=express();
+
+
+app.use(bodyParser.json());
+app.post('/todos',(req, res)=>{
+    //console.log(req.body);
+    var newTodo=new Todo({
+        text:req.body.text
+        //completed:false,
+        //completedAt:10
+    });
+    
+    newTodo.save().then((doc)=>{
+    
+        res.send(doc);
+    },(e)=>{
+            res.status(400).send(e);
+    });
+});
+
+app.listen(3000);
+
+/*var newTodo2=new Todo({
+    text:'    Play Chess 2   '
+    //completed:false,
+    //completedAt:10
+});
+
+newTodo2.save().then((doc)=>{
+
+    console.log('Saved Todo',doc);
+},(e)=>{
+        console.log('unable to do');
+});*/
