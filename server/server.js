5a319efd1b98eb55524357e95eae893f67269167
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-07-25 15:54:57 +0530

    GET and POST req MongoDb

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ app.post('/todos',(req, res)=>{
     });
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    },(e)=>{
+        res.status(400).send(e);
+})
+
+});
+
 app.listen(3000);
 
 /*var newTodo2=new Todo({
