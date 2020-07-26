798abf96d0cd733430362b9809a3096236521389
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-07-26 16:40:39 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var {Todo}=require('./models/todo');
 var express=require('express');
 var bodyParser=require('body-parser');
 const {ObjectID}=require('mongodb');
-
+const _=require('lodash');
 var app=express();
 
 const port=process.env.PORT || 3000;
@@ -69,6 +69,35 @@ app.delete('/todos/:id',(req,res)=>{
             res.send({todo});
         }).catch((e)=>{res.status(400).send();console.log(e);});
     }
+});
+
+
+app.patch('/todos/:id',(req,res)=>{
+        var id=req.params.id;
+        var body=_.pick(req.body,['text','completed']);
+        if(!ObjectID.isValid(id))
+    {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed)
+    {
+        body.completedAt=new Date().getTime();
+    }
+    else{
+        body.completed=false;
+        body.completedAt=null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+            if(!todo)
+            {
+                return res.status(404).send();
+            }
+            res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
 });
 
 app.listen(port,()=>{
